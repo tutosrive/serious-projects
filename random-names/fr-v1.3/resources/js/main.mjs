ea4b237a7969c98aa45c8167a3e235dba0551493
tutosrivelq <tutosrivegamer@gmail.com>
2025-01-07 18:47:42 -0500

    fix some errors in main.mjs and index.html

--- random-names/fr-v1.3/resources/js/main.mjs
+++ random-names/fr-v1.3/resources/js/main.mjs
@@ -1,4 +1,4 @@
-import Imports from './imports.mjs';
+import Imports from "./imports.mjs";
 
 class App {
   static async init() {
@@ -7,7 +7,7 @@ class App {
       await App.#listener();
       await Manager.loadHome();
     } catch (e) {
-      Toast.show({ message: e.message, mode: 'danger', error: e });
+      Toast.show({ message: e.message, mode: "danger", error: e });
     }
   }
 
@@ -15,8 +15,8 @@ class App {
    * "Escucha" escucha/reacciona a los eventos del menú de navegación (header a)
    */
   static async #listener() {
-    document.querySelectorAll('header a').forEach(ancla => {
-      ancla.addEventListener('click', async e => {
+    document.querySelectorAll("header a").forEach((ancla) => {
+      ancla.addEventListener("click", async (e) => {
         e.preventDefault();
 
         // Manager.loadContainer();
@@ -24,28 +24,28 @@ class App {
         let reg;
 
         // Si el target es un IMG, reg será su id
-        if (e.target.tagName === 'IMG') {
+        if (e.target.tagName === "IMG") {
           reg = e.target.id;
         }
         // Si es un ancla, tendrá un href el cual se verificará con la regex
-        else if (e.target.tagName === 'A') {
-          reg = e.target.href.match('[^/]+(?=/?$)')[0];
+        else if (e.target.tagName === "A") {
+          reg = e.target.href.match("[^/]+(?=/?$)")[0];
         }
 
         // Validar opciones
         switch (reg) {
           // Cargar página de inicio
-          case 'home':
+          case "home":
             await Manager.loadHome();
             break;
           // Códigos válidos
-          case 'es-MX': // Español (México)
-          case 'de-CH': // Alemania
-          case 'es-ES': // Español (España)
-          case 'en-US': // Inglés (Estados Unidos)
-          case 'fr-FR': // Francés (Francia)
-          case 'it-IT': // Italiano (Italia)
-          case 'pt-BR': // Portugués (Brasil)
+          case "es-MX": // Español (México)
+          case "de-CH": // Alemania
+          case "es-ES": // Español (España)
+          case "en-US": // Inglés (Estados Unidos)
+          case "fr-FR": // Francés (Francia)
+          case "it-IT": // Italiano (Italia)
+          case "pt-BR": // Portugués (Brasil)
             await Manager.sendData(`${urlAPI}/person/41/${reg}`);
             break;
         }
@@ -54,4 +54,4 @@ class App {
   }
 }
 
-App.init();
+await App.init();
